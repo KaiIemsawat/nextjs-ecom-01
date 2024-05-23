49f70053c7c485aa3d595cf703164d0c617a540b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-23 09:58:02 -0400

    card - customers

--- e-com/src/app/admin/page.tsx
+++ e-com/src/app/admin/page.tsx
@@ -19,14 +19,55 @@ const getSalesData = async () => {
     };
 };
 
+// const getUserData = async () => {
+//     const userCount = await db.user.count();
+//     const orderData = await db.order.aggregate({
+//         _sum: { pricePaidInCent: true },
+//     });
+//     return {
+//         userCount,
+//         averageValuePerUser:
+//             userCount === 0
+//                 ? 0
+//                 : (orderData._sum.pricePaidInCent || 0) / userCount / 100,
+//     };
+// };
+// * Switch to below block of code so we don't need 2 awaits
+const getUserData = async () => {
+    const [userCount, orderData] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({
+            _sum: { pricePaidInCent: true },
+        }),
+    ]);
+    return {
+        userCount,
+        averageValuePerUser:
+            userCount === 0
+                ? 0
+                : (orderData._sum.pricePaidInCent || 0) / userCount / 100,
+    };
+};
+
 const AdminDashboard = async () => {
-    const salesData = await getSalesData();
+    const [salesData, userData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+    ]);
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <DashboardCard
-                title="sales"
-                subtitle={formatNumber(salesData.numberOfSales)}
+                title="Sales"
+                subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
                 body={formatCurrency(salesData.amount)}
+            />
+            <DashboardCard
+                title="Customers"
+                subtitle={`${formatCurrency(
+                    userData.averageValuePerUser
+                )} Average Value`}
+                body={formatNumber(userData.userCount)}
             />
         </div>
     );
