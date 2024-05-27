a7728f9e204ec4d13a35e7ab44a9b1b81e07cd5b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-26 21:46:05 -0400

    edit product (base)

--- e-com/src/app/admin/products/_components/ProductForm.tsx
+++ e-com/src/app/admin/products/_components/ProductForm.tsx
@@ -8,8 +8,9 @@ import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
 import { addProduct } from "../../_actions/products";
 import { useFormState, useFormStatus } from "react-dom";
+import { Product } from "@prisma/client";
 
-const ProductForm = () => {
+const ProductForm = ({ product }: { product?: Product | null }) => {
     const [error, action] = useFormState(addProduct, {});
 
     const [priceInCents, setPriceInCents] = useState<number>();
