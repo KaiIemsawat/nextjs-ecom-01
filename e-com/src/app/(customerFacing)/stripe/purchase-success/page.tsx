5c271dcd349405af0570295c6f7128e7b08730dc
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-29 22:09:05 -0400

    payment success page, download link (base)

--- e-com/src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ e-com/src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import db from "@/db/db";
 import { formatCurrency } from "@/lib/formatters";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
 
@@ -29,7 +31,9 @@ const SuccessPage = async ({
 
     return (
         <div className="max-w-5xl w-full mx-auto space-y-8">
-            <h1>{isSuccess ? "Success!" : "Error!"}</h1>
+            <h1 className="text-4xl font-bold">
+                {isSuccess ? "Success!" : "Error!"}
+            </h1>
             <div className="flex gap-4 items-center">
                 <div className="aspect-video flex-shrink-0 w-1/3 relative">
                     <Image
@@ -47,9 +51,35 @@ const SuccessPage = async ({
                     <div className="line-clamp-3 text-muted-foreground">
                         {product.description}
                     </div>
+                    <Button className="mt-4" size="lg" asChild>
+                        {isSuccess ? (
+                            <a
+                                href={`/products/download/${await createDownloadVerification(
+                                    product.id
+                                )}`}
+                            >
+                                Download
+                            </a>
+                        ) : (
+                            <Link href={`/products/${product.id}/purchase`}>
+                                Try Again
+                            </Link>
+                        )}
+                    </Button>
                 </div>
             </div>
         </div>
     );
 };
 export default SuccessPage;
+
+const createDownloadVerification = async (productId: string) => {
+    return (
+        await db.downloadVerification.create({
+            data: {
+                productId,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+            },
+        })
+    ).id;
+};
